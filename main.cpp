53d1637d0250e5814da08f74be7dbd4daf92e927
Elmer Mosquera <elmermosquera72@gmail.com>
2026-03-19 09:01:43 -0500

    Se agrega mensaje de retroalimentacion, en caso de ingresar numero distinto al esperado

--- main.cpp
+++ main.cpp
@@ -13,12 +13,18 @@ void ingresarDimencionesTablero(){
     do {
         cout << "Ingrese la altura del tablero que desea\n(SOLO MULTIPLOS DE 8 'OHCO')\n>> ";
         cin >> altura;
+        if ((altura&7)!=0) {
+            cout << "El tamaño de alto que inseste no es valido (solo multiplos de 8)" << endl;
+        }
     } while ((altura&7)!=0);
 
     do {
 
         cout << "Ingrese la ancho del tablero que desea\n(SOLO MULTIPLOS DE 8 'OHCO')\n>> ";
         cin >> ancho;
+        if ((ancho&7)!=0) {
+            cout << "El tamaño de ancho que inseste no es valido (solo multiplos de 8)" << endl;
+        }
     } while ((ancho&7)!=0);
 
 }
