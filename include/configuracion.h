1c0f94839e7cd7bde7d88ba397f2ad9ff645d7f8
Elmer Mosquera <elmermosquera72@gmail.com>
2026-03-19 10:08:10 -0500

    clasificacion de funcion ingresarDimencinesTablero

---
+++ include/configuracion.h
@@ -0,0 +1,7 @@
+// funciones.h
+#ifndef CONFIGURACION_H
+#define CONFIGURACION_H
+
+    void ingresarDimencionesTablero();
+
+#endif
--- main.cpp
+++ main.cpp
@@ -1,30 +1,4 @@
-#include <iostream>
-
-using namespace std;
-
-void ingresarDimencionesTablero(){
-    int altura;
-    int ancho;
-
-
-    do {
-        cout << "Ingrese la altura del tablero que desea\n(SOLO MULTIPLOS DE 8 'OHCO')\n>> ";
-        cin >> altura;
-        if ((altura&7)!=0) {
-            cout << "El tamaño de alto que inseste no es valido (solo multiplos de 8)" << endl;
-        }
-    } while ((altura&7)!=0);
-
-    do {
-
-        cout << "Ingrese la ancho del tablero que desea\n(SOLO MULTIPLOS DE 8 'OHCO')\n>> ";
-        cin >> ancho;
-        if ((ancho&7)!=0) {
-            cout << "El tamaño de ancho que inseste no es valido (solo multiplos de 8)" << endl;
-        }
-    } while ((ancho&7)!=0);
-
-}
+#include "configuracion.h"
 
 int main()
 {
