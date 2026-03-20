af64203610f51542f906b61f844f34d7f672b950
Elmer Mosquera <elmermosquera72@gmail.com>
2026-03-19 23:39:23 -0500

    feat: Se agrega la funcion mostrarEstadoDeTablero function

--- include/frontend.h
+++ include/frontend.h
@@ -1,6 +1,10 @@
 #ifndef FRONTEND_H
 #define FRONTEND_H
 
-void limpiarTablero();
 
-#endif
+
+    void limpiarTablero();
+    void mostrarEstadoDeTablero(int columnas,int filas);
+
+
+#endif // DEBU
--- src/frontend.cpp
+++ src/frontend.cpp
@@ -2,9 +2,19 @@
 
 using namespace std;
 
+
 int LimpiarFilaEnElTablero(){
     cout << "limpiar fila" << endl;
 
 return 0;
 }
 
+void mostrarEstadoDeTablero(int filas,int columnas){
+
+    for (int fila = 1; fila <= filas; fila++){
+        for (int elemento = 1; elemento <= columnas; elemento++){
+            cout << elemento << " ";
+        }
+        cout<<endl; // Aqui hay un salto de linea
+    }
+}
